ba0bb40eb4774d69919573a6465134a6b88f287d
Paramesh Chandra <parameshchandra28@gmail.com>
2021-03-14 13:00:45 +0000

    imaginary root .cpp update and complete

--- Numerical_root_finding/Newton-Raphson-Imaginary-root/newton-raphson-imaginary.cpp
+++ Numerical_root_finding/Newton-Raphson-Imaginary-root/newton-raphson-imaginary.cpp
@@ -44,37 +44,28 @@ float dhy(float x,float y)
 
 int main()
 {
-  float x0,x,y,y0,x1,y1,denom,del_x,del_y,x3,y3;
-  ofstream out1("newrap_z2reg1.out");
-  ofstream out2("newrap_z2reg2.out");
-  for(x=-2.0;x<=2.0;x=x+0.01)
+  float x0,x,y,y0,x1,y1,denom,del_x,del_y;
+  //ofstream out1("newrap_z2reg1.out");
+  //ofstream out2("newrap_z2reg2.out");
+  cout<<"Input the guess value for real solution: ";
+  cin>>x;
+  cout<<"Input the guess value for imaginary solution: ";
+  cin>>y;
+  x0 = x;
+  y0 = y;
+  do
   {
-    for(y=-2.0;y<=2.0;y=y+0.01)
-    {
-      x3 = x;
-      y3 = y;
-      x0 = x;
-      y0 = y;
-      do
-      {
-        denom = dgx(x0,y0)*dhy(x0,y0)-dgy(x0,y0)*dhx(x0,y0);
-        del_x = (dgx(x0,y0)*h(x0,y0)-g(x0,y0)*dhy(x0,y0))/denom;
-        del_y = (g(x0,y0)*dhx(x0,y0)-dgy(x0,y0)*h(x0,y0))/denom;
-        x1 = x0 + del_x;
-        y1 = y0 + del_y;
-        x0 = x1;
-        y0 = y1;
-        cout<<x0<<'\t'<<y0<<endl;
-      }
-      while(abs(del_x)>0.00001 || abs(del_y)>0.00001);
-      cout<<'End of itteration 1'<<endl;
-    }
-    cout<<'end of itteration 2'<<endl;
+    denom = dgx(x0,y0)*dhy(x0,y0)-dgy(x0,y0)*dhx(x0,y0);
+    del_x = (dgy(x0,y0)*h(x0,y0)-g(x0,y0)*dhy(x0,y0))/denom;
+    del_y = (g(x0,y0)*dhx(x0,y0)-dgx(x0,y0)*h(x0,y0))/denom;
+    x1 = x0 + del_x;
+    y1 = y0 + del_y;
+    x0 = x1;
+    y0 = y1;
+    //cout<<x0<<"\t"<<y0<<endl;
+    //cout<<del_x<<"\t"<<del_y<<endl;
   }
-  cout<<"the root is "<<x0<<" +i("<<y0<<") "<<denom<<endl;
-  if(abs(x0+0.0)<0.001 && abs(y0)<0.001)
-  out1<<x3<<" "<<y3<<endl;
-  if(abs(x0+0.0)<0.001 && abs(y0)<0.001)
-  out2<<x3<<" "<<y3<<endl;
+  while(abs(g(x0,y0))>0.000001 || abs(h(x0,y0))>0.000001);
+  cout<<"the root is ("<<x0<<") + i ("<<y0<<")"<<endl;
 }
 
