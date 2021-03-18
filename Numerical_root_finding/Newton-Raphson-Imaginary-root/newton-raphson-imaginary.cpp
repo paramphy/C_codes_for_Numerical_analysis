78fc5575de69b34a4779b53c548db7cc86641f1a
Paramesh Chandra <parameshchandra28@gmail.com>
2021-03-18 14:24:54 +0000

    imaginary update

--- Numerical_root_finding/Newton-Raphson-Imaginary-root/newton-raphson-imaginary.cpp
+++ Numerical_root_finding/Newton-Raphson-Imaginary-root/newton-raphson-imaginary.cpp
@@ -53,19 +53,23 @@ int main()
   cin>>y;
   x0 = x;
   y0 = y;
-  do
-  {
-    denom = dgx(x0,y0)*dhy(x0,y0)-dgy(x0,y0)*dhx(x0,y0);
-    del_x = (dgy(x0,y0)*h(x0,y0)-g(x0,y0)*dhy(x0,y0))/denom;
-    del_y = (g(x0,y0)*dhx(x0,y0)-dgx(x0,y0)*h(x0,y0))/denom;
-    x1 = x0 + del_x;
-    y1 = y0 + del_y;
-    x0 = x1;
-    y0 = y1;
-    //cout<<x0<<"\t"<<y0<<endl;
-    //cout<<del_x<<"\t"<<del_y<<endl;
-  }
+  denom = dgx(x0,y0)*dhy(x0,y0)-dgy(x0,y0)*dhx(x0,y0);
+  if(denom == 0)
+    cout<<"Please change the initial guess values"<<endl;
+  else{
+    do
+    {
+      denom = dgx(x0,y0)*dhy(x0,y0)-dgy(x0,y0)*dhx(x0,y0);
+      del_x = (dgy(x0,y0)*h(x0,y0)-g(x0,y0)*dhy(x0,y0))/denom;
+      del_y = (g(x0,y0)*dhx(x0,y0)-dgx(x0,y0)*h(x0,y0))/denom;
+      x1 = x0 + del_x;
+      y1 = y0 + del_y;
+      x0 = x1;
+      y0 = y1;
+      //cout<<x0<<"\t"<<y0<<endl;
+      //cout<<del_x<<"\t"<<del_y<<endl;
+    }
   while(abs(g(x0,y0))>0.000001 || abs(h(x0,y0))>0.000001);
   cout<<"the root is ("<<x0<<") + i ("<<y0<<")"<<endl;
+  }
 }
-
