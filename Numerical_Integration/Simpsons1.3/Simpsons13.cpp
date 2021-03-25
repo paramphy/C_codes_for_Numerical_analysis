b75c9db63d2f7ed956694cef9caa15d50c60c75c
Paramesh Chandra <parameshchandra28@gmail.com>
2021-03-25 14:33:49 +0000

    simpson13

---
+++ Numerical_Integration/Simpsons1.3/Simpsons13.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<cmath>
+#include<fstream>
+
+using namespace std;
+
+float f(float x)
+{
+  return(sin(x));
+}
+
+int main(void)
+{
+  int n,i;
+  float a,b,h,x[100],y[100],s,s1,s2=0,s3=0;
+  cout<<"Give the \n lower limit: ";
+  ofstream out1("simp13.out");
+  cin>>a;
+  cout<<"Upper limit: ";
+  cin>>b;
+  cout<<"Enter the even number of interval:";
+  cin>>n;
+  h=(b-a)/n;
+  for(i=0;i<n;i++)
+  {
+    x[i]=a+i*h;
+    y[i]=f(x[i]);
+    out1<<x[i]<<" \t"<<y[i]<<endl;
+  }
+  s1=f(a)+f(b);
+  for(i=3;i<=n;i=i+2)
+  {
+    s2=s2+4*f(a+i*h);
+    s3=s3+2*f(a+(i-1)*h);
+  }
+  s=s1+s2+s3;
+  s=(h/3.0)*s;
+  cout<<"The resultent integral value: "<<s<<endl;
+  return 0;
+}
